37d0ccbc550bfbe440c8348b80908233e85e49ee
eyuel abebe <eyueld@gmail.com>
2016-08-20 23:35:38 -0400

    initialized app

---
+++ ext-module/psFramework/psFrameworkDirective.js
@@ -0,0 +1,14 @@
+"use strict";
+
+angular.module("psFramework").directive("psFramework", function(){
+  return {
+      transclude: false,
+      scope: {
+        title: '@',
+        subtitle: '@',
+        iconFile: '@'
+      },
+      controller: "psFrameworkController",
+      templateUrl: "ext-module/psFramework/psFrameworkTemplate.html"
+  };
+});
